9bfcded39f773369507fad92404bb039d048ea9e
Johannes Plunien <plu@pqpq.de>
2014-06-14 10:19:08 +0200

    Add RNObject.

---
+++ ReactiveNetworking/RNObject.h
@@ -0,0 +1,13 @@
+//
+//  RNObject.h
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface RNObject : MTLModel <MTLJSONSerializing>
+
+@end
---
+++ ReactiveNetworking/RNObject.m
@@ -0,0 +1,13 @@
+//
+//  RNObject.m
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import "RNObject.h"
+
+@implementation RNObject
+
+@end
