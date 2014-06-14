982c972bdb859bc9fa500aaa6dcfb4aa7dd599a8
Johannes Plunien <plu@pqpq.de>
2014-06-14 10:24:47 +0200

    Add RNResponse.

---
+++ ReactiveNetworking/RNResponse.h
@@ -0,0 +1,13 @@
+//
+//  RNResponse.h
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+@interface RNResponse : MTLModel
+
+@end
---
+++ ReactiveNetworking/RNResponse.m
@@ -0,0 +1,13 @@
+//
+//  RNResponse.m
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import "RNResponse.h"
+
+@implementation RNResponse
+
+@end
