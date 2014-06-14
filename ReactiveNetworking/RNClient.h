6f95c01787e8b6b6f3524f7e984abdcb8d829b54
Johannes Plunien <plu@pqpq.de>
2014-06-14 09:18:49 +0200

    Add RNClient.

---
+++ ReactiveNetworking/RNClient.h
@@ -0,0 +1,13 @@
+//
+//  RNClient.h
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface RNClient : AFHTTPClient
+
+@end
---
+++ ReactiveNetworking/RNClient.m
@@ -0,0 +1,13 @@
+//
+//  RNClient.m
+//  ReactiveNetworking
+//
+//  Created by Johannes Plunien on 14/06/14.
+//  Copyright (c) 2014 Johannes Plunien. All rights reserved.
+//
+
+#import "RNClient.h"
+
+@implementation RNClient
+
+@end
