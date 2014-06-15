e19010d5f7cc4b15baa8c51db45b78e5eab70286
Johannes Plunien <plu@pqpq.de>
2014-06-15 08:01:24 +0200

    Add Mantel methods.

--- ReactiveNetworking/RNObject.h
+++ ReactiveNetworking/RNObject.h
@@ -10,4 +10,12 @@
 
 @interface RNObject : MTLModel <MTLJSONSerializing>
 
+// The unique ID for this object. This is only guaranteed to be unique among
+// objects of the same type, from the same server.
+//
+// By default, the JSON representation for this property assumes a numeric
+// representation (which is the case for most API objects). Subclasses may
+// override the `+objectIDJSONTransformer` method to change this behavior.
+@property (nonatomic, copy, readonly) NSString *objectID;
+
 @end
